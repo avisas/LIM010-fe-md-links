d1b5144a299fc1c72bd99c68bb2addacf521d28f
avisas <alejandra.visa91@gmail.com>
2019-10-17 00:11:12 -0500

    functions names

--- src/main.js
+++ src/main.js
@@ -2,36 +2,36 @@ import { isMdFile, isDirectory, getAbsolutePath } from "./files-path";
 
 const inputUserPath = 'c:/users/Alejandra/Downloads/myPath';
 const options = {
-    stats: false,
-    validate: false
+  stats: false,
+  validate: false
 };
 
 const main = (inputUserPath, options) => {
-    const listOfMarkdownFiles = [];
-    updateListOfMarkdownFiles(listOfMarkdownFiles, inputUserPath); // ['absPath1/file1.md', 'absPath2/file2.md', ...]
-    const listOfURLProperties = getListOfURLProperties(listOfMarkdownFiles);
-    printConsole(listOfURLProperties, options);
+  const listOfMarkdownFiles = [];
+  updateListOfMarkdownFiles(listOfMarkdownFiles, inputUserPath); // ['absPath1/file1.md', 'absPath2/file2.md', ...]
+  const listOfURLProperties = getListOfURLProperties(listOfMarkdownFiles);
+  printConsole(listOfURLProperties, options);
 };
 
 const updateListOfMarkdownFiles = (listOfMarkdownFiles = [], path = '') => {
-    if (isMdFile(path)) {
-        listOfMarkdownFiles.push(getAbsolutePath(path));
-    } else if (isDirectory(path)) {
-        path.forEachFile(childPath => updateListOfMarkdownFiles(listOfMarkdownFiles, childPath));
-    }; // si es un directory, ahora listar todos sus archivos o carpetas hijo.
+  if (isMdFile(path)) {
+    listOfMarkdownFiles.push(getAbsolutePath(path));
+  } else if (isDirectory(path)) {
+    path.forEachFile(childPath => updateListOfMarkdownFiles(listOfMarkdownFiles, childPath));
+  }; // si es un directory, ahora listar todos sus archivos o carpetas hijo.
 };
 
 const getListOfURLProperties = (markdownFiles = []) => {
-    const output = [];
-    markdownFiles.forEach(mdFilePath => {
-        unProcessedListOfNamesAndURLs = getListOfURLs(mdFilePath.content()); //acceder a todo su contenido
-        unProcessedListOfNamesAndURLs.forEach(nameAndURL => {
-            output.push(getURLFinalObject(mdFilePath, nameAndURL));
-        });
+  const output = [];
+  markdownFiles.forEach(mdFilePath => {
+    unProcessedListOfNamesAndURLs = getListOfURLs(mdFilePath.content()); //acceder a todo su contenido
+    unProcessedListOfNamesAndURLs.forEach(nameAndURL => {
+      output.push(getURLFinalObject(mdFilePath, nameAndURL));
     });
-    return output;
+  });
+  return output;
 };
 
-export {main, updateListOfMarkdownFiles, getListOfURLProperties };
+export { main, updateListOfMarkdownFiles, getListOfURLProperties };
 
 
