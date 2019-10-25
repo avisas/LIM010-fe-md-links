5fd09b131ab8e4ce9ef7cfeeff9d4318408bc34a
avisas <alejandra.visa91@gmail.com>
2019-10-25 15:41:22 -0500

    link in cli.spec.js

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -8,21 +8,21 @@ fetchMock.config.sendAsJson = false;
 const output1 = `
 ${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://www.markdownguide.org/getting-started/')} ${chalk.yellow('1')}
 ${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://developer.mozilla.org/es/docs/Web/JavaScript')} ${chalk.yellow('2')}
-${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://hackwild.com/article/creating-a-command')} ${chalk.yellow('3')}
+${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://hackwildm/article/creating-a-command')} ${chalk.yellow('3')}
 ${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://medium.com/@josephcardillo/the-difference')} ${chalk.yellow('4')}`;
 const output2 = `\n${chalk.cyan('Total: ')} 4 \n${chalk.cyan('Unique: ')} 4`;
 
 const output3 = `
 ${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://www.markdownguide.org/getting-started/')} ${chalk.green('200')} ${chalk.bgGreen.black('OK')} ${chalk.yellow('1')}
 ${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://developer.mozilla.org/es/docs/Web/JavaScript')} ${chalk.green('200')} ${chalk.bgGreen.black('OK')} ${chalk.yellow('2')}
-${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://hackwild.com/article/creating-a-command')} ${chalk.green('200')} ${chalk.bgGreen.black('OK')} ${chalk.yellow('3')}
+${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://hackwildm/article/creating-a-command')} ${chalk.green('200')} ${chalk.bgGreen.black('OK')} ${chalk.yellow('3')}
 ${chalk.bgBlue.black(path.join(process.cwd(), 'dir-test', 'first.md'))} ${chalk.magenta('https://medium.com/@josephcardillo/the-difference')} ${chalk.red('404')} ${chalk.bgRed.black('FAIL')} ${chalk.yellow('4')}`;
 
 describe('Command line', () => {
   fetchMock
     .mock('https://www.markdownguide.org/getting-started/', 200)
     .mock('https://developer.mozilla.org/es/docs/Web/JavaScript', 200)
-    .mock('https://hackwild.com/article/creating-a-command', 'ERR')
+    .mock('https://hackwildm/article/creating-a-command', 'ERR')
     .mock('https://medium.com/@josephcardillo/the-difference', 404);
 
   it('Debería retornar function', () => {
