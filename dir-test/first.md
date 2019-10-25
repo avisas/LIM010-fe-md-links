7ea4c018fec763448e251c567ea80c8b9aa17ef9
avisas <alejandra.visa91@gmail.com>
2019-10-25 15:38:45 -0500

    link modified in validate.spec.js

--- dir-test/first.md
+++ dir-test/first.md
@@ -1,5 +1,5 @@
 [1](https://www.markdownguide.org/getting-started/)
 Primer ejemplo de Md
 [2](https://developer.mozilla.org/es/docs/Web/JavaScript)
-[3](https://hackwild.com/article/creating-a-command)
+[3](https://hackwildm/article/creating-a-command)
 [4](https://medium.com/@josephcardillo/the-difference)
--- test/validate.spec.js
+++ test/validate.spec.js
@@ -11,7 +11,7 @@ const output = [
     href: 'https://developer.mozilla.org/es/docs/Web/JavaScript', path: path.join(process.cwd(), 'dir-test', 'first.md'), status: 200, statusText: 'OK', text: '2',
   },
   {
-    href: 'https://hackwild.com/article/creating-a-command', path: path.join(process.cwd(), 'dir-test', 'first.md'), status: 'ERR', statusText: 'FAIL', text: '3',
+    href: 'https://hackwildm/article/creating-a-command', path: path.join(process.cwd(), 'dir-test', 'first.md'), status: 'ERR', statusText: 'FAIL', text: '3',
   },
   {
     href: 'https://medium.com/@josephcardillo/the-difference', path: path.join(process.cwd(), 'dir-test', 'first.md'), status: 404, statusText: 'FAIL', text: '4',
@@ -21,7 +21,7 @@ describe('Validate link', () => {
   fetchMock
     .mock('https://www.markdownguide.org/getting-started/', 200)
     .mock('https://developer.mozilla.org/es/docs/Web/JavaScript', 200)
-    .mock('https://hackwild.com/article/creating-a-command', () => {
+    .mock('https://hackwildm/article/creating-a-command', () => {
       throw new Error('ERROR_MESSAGE');
     })
     .mock('https://medium.com/@josephcardillo/the-difference', 404);
